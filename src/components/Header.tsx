1f731849a85139fcaa544f5708e6434f131d4b4f
whatcodehaveyougit <sigurdwatt@gmail.com>
2024-03-06 14:03:04 +0000

    Install sass and make page scss files

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,15 @@ import { Link } from 'react-router-dom';
 
 function Header(): JSX.Element {
   return (
-    <div>
-      <h1>SIGURD WATT</h1>
-      <h2>SOFTWARE DEVELOPER</h2>
-      <div>
-        <a href="tel:07724543520">07724 543 520</a>
-        <a href="mailto:sigurdwatt@gmail.com?subject-Contact">sigurdwatt@gmail.com</a>
+    <div className="header">
+      <div className="hero">
+        <h1>SIGURD WATT</h1>
+        <h2>SOFTWARE DEVELOPER</h2>
+        <div className="contact-details">
+          <a href="tel:07724543520">07724 543 520</a>
+          <span>|</span>
+          <a href="mailto:sigurdwatt@gmail.com?subject-Contact">sigurdwatt@gmail.com</a>
+        </div>
       </div>
       <nav>
         <ul id="menu">
